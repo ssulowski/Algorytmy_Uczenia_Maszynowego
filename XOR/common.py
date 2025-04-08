e1464145a057b9cce8f6916647aee129592d23d9
Szymon Sułowski <128406275+ssulowski@users.noreply.github.com>
2025-04-08 21:26:22 +0200

    Update common.py

--- XOR/common.py
+++ XOR/common.py
@@ -25,15 +25,3 @@ def plot_weights(model: tf.keras.Sequential) -> None:
             plt.ylabel("Frequency")
             plt.grid(True)
             plt.show()
-        elif len(weights[i].shape) == 4:
-            num_filters = weights[i].shape[3]
-            plt.figure(figsize=(10, 5))
-            for j in range(num_filters):
-                plt.subplot(num_filters // 4 + 1, 4, j + 1)
-                plt.hist(weights[i][:, :, :, j].flatten(), bins=30)
-                plt.title("Filter {} - Layer {}".format(j + 1, i))
-                plt.xlabel("Weight value")
-                plt.ylabel("Frequency")
-                plt.grid(True)
-            plt.tight_layout()
-            plt.show()
